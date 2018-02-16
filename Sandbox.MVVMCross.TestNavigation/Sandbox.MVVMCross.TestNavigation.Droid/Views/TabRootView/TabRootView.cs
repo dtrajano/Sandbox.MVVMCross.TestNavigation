1cd1453829bed64052e4efe0d1ce4f8562007091
Rafael Goulart Cardoso <rgcardoso@stefanini.com>
2018-02-16 17:29:30 -0200

    Alteracao de layout a tabview do android.

--- Sandbox.MVVMCross.TestNavigation/Sandbox.MVVMCross.TestNavigation.Droid/Views/TabRootView/TabRootView.cs
+++ Sandbox.MVVMCross.TestNavigation/Sandbox.MVVMCross.TestNavigation.Droid/Views/TabRootView/TabRootView.cs
@@ -8,6 +8,7 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
@@ -25,11 +26,39 @@ namespace Sandbox.MVVMCross.TestNavigation.Droid.Views
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.TabRootView);
+            SetContentView(Resource.Layout.TabRootView);            
 
             if (bundle == null)
             {
                 ViewModel.ShowInitialViewModelsCommand.Execute();
+            }
+        }
+
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+
+            setupIconsTabBar();
+        }
+
+        private void setupIconsTabBar()
+        {
+            var tabLayout = (TabLayout)FindViewById(Resource.Id.tabs);
+
+            for (int i = 0; i < tabLayout.TabCount; i++)
+            {
+                if (i == 0)
+                {
+                    tabLayout.GetTabAt(0).SetIcon(Resource.Drawable.icn_home);
+                }
+                else if (i == 1)
+                {
+                    tabLayout.GetTabAt(1).SetIcon(Resource.Drawable.icn_minha_lider);
+                }
+                else
+                {
+                    tabLayout.GetTabAt(2).SetIcon(Resource.Drawable.icn_preferencias);
+                }
             }
         }
     }
